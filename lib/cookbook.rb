9ac66a5a2c52e3d7779e66fd0b40135c1d59dbc4
Arbi Velaj <arbi.velaj@gmail.com>
2023-04-19 18:02:03 +0100

    tesgt

---
+++ lib/cookbook.rb
@@ -0,0 +1,11 @@
+# initialize a class called cookbook with a csv file path
+# initialize an array of recipes to store the recipes made form the csv
+# load from the csv each line and use it to create an instance of recipe
+
+# https://kitt.lewagon.com/camps/1176/lectures/01-Ruby%2F06-Parsing#slides0
+# putting it into the recipes array
+
+# after this, add the add_recipe method and the all_recipe method
+
+# when adding a recipe the to the cookbook we need to update the csv
+
